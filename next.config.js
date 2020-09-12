53d6e8ed38e2fb3131d89c30f8644032e960646e
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-09-12 17:07:09 +0200

    next: use git sha as build id

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const { findLastCommitSha } = require('./gitBuildId');
+
 module.exports = {
   distDir: 'build',
+  generateBuildId: async () => {
+    const commitSha = await findLastCommitSha();
+    return commitSha.substr(0, 7);
+  },
 };
