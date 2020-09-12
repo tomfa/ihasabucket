17a66ace9cab4f35f892b3d996b26eac134b5451
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-09-12 22:48:10 +0200

    lint: tweak eslint react-rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,53 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
+    // React's PropTypes are discouraged
+    'react/require-default-props': 0,
+    'react/no-unused-prop-types': 0,
+    'react/prop-types': 0,
+
+    // Only for styling/consistency, often with undesirable defaults.
+    // Can be considered in individual repos.
+    'react/jsx-curly-brace-presence': 0,
+    'react/static-property-placement': 0,
+    'react/destructuring-assignment': 0,
+    'react/state-in-constructor': 0,
+
+    // Does not support dynamic type prop, which we want.
+    // ref https://github.com/yannickcr/eslint-plugin-react/issues/1555
+    'react/button-has-type': 0,
+
+    // Allow JSX in .js files
+    'react/jsx-filename-extension': [0, { extensions: ['.jsx'] }],
+
+    // Ordering of react component methods
+    'react/sort-comp': [
+      1,
+      {
+        order: [
+          'type-annotations',
+          'static-methods',
+          'lifecycle',
+          'everything-else',
+          'render',
+        ],
+      },
+    ],
+    'jsx-a11y/label-has-for': [
+      2,
+      {
+        required: {
+          every: ['id'],
+        },
+        allowChildren: false,
+      },
+    ],
+
+    // Allows label-control connection being nested in DOM
+    'jsx-a11y/label-has-associated-control': [2, { assert: 'either' }],
+
+    'react/no-did-mount-set-state': [0],
+
     // Do not allow use of undefined variables
     'no-undef': 2,
 
@@ -66,12 +113,6 @@ module.exports = {
 
     // I can use spreading carefully, thank you :)
     'react/jsx-props-no-spreading': 0,
-
-    // I am using jsx in more files than .jsx
-    'react/jsx-filename-extension': [
-      2,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
 
     // We use short-circuit and ternary function calls, though they can be less
     //   readable. Could be considered removed at a later stage.
