39ddbe3586e270b8efc835f89a707b8b864bfe65
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-09-13 22:52:11 +0200

    Meta: add meta tags

--- components/MetaTags/Meta.tsx
+++ components/MetaTags/Meta.tsx
@@ -2,7 +2,21 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
-    <title>ihasabucket</title>
+    <title>ihasabucket - instant web app hosting</title>
+    <meta
+      property="og:image"
+      content="https://user-images.githubusercontent.com/1502702/92998877-e9e1cc00-f51c-11ea-98e6-331460921cde.jpg"
+    />
+    <meta property="og:url" content="https://ihasabucket.it" />
+    <meta
+      property="og:title"
+      content="I has a bucket - instant web app hosting"
+    />
+    <meta
+      property="og:description"
+      content="Guide for hosting React, Vue, Gatsby or user uploads on AWS S3 buckets"
+    />
+
     <link rel="icon" href="/favicon.ico" />
     <link
       href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@600&display=swap"
