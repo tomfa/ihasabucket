fe4ed1e931e608be0aedfc34855b2a81613a6b1b
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-09-15 21:48:57 +0200

    styling: fix overflow ui bug

--- components/utils.tsx
+++ components/utils.tsx
@@ -13,12 +13,9 @@ export const SectionBackground = styled.div`
 
 export const Section = styled.section`
   max-width: ${(p) => p.theme.layout.maxWidth}px;
-  width: 100vw;
+  width: 100%;
   margin: 0 auto;
-  padding-top: 2rem;
-  padding-bottom: 2rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
+  padding: 2rem 1rem;
   position: relative;
 
   @media (min-width: 375px) {
@@ -30,7 +27,7 @@ export const Section = styled.section`
 export const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100vw;
+  width: 100%;
 `;
 
 export const Colored = styled.span`
