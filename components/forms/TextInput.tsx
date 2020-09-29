de5b68c8f490ba3a67f5278834483efbcf60fdd2
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-09-29 23:31:21 +0200

    TextInput: clean up casing and spaces
    TextInput now disallows casing and spaces. Might
    want to go back on this later.

--- components/forms/TextInput.tsx
+++ components/forms/TextInput.tsx
@@ -28,7 +28,7 @@ const TextInput = ({
   description,
 }: Props) => {
   const [inputValue, setInputValue] = useState<string>(value || '');
-  const [hasSubmitted, setSubmitted] = useState<boolean>(false);
+  const [hasSubmitted, setSubmitted] = useState<boolean>(!!value);
   const [placeholderIndex, setPlaceholderIndex] = useState<number>(
     placeholders?.length ? 0 : -1
   );
@@ -36,9 +36,11 @@ const TextInput = ({
     placeholderIndex === -1 ? placeholder : placeholders[placeholderIndex]
   );
   useEffect(() => {
-    if (value !== null && value !== undefined) {
-      setInputValue(value);
+    if (value) {
       setSubmitted(true);
+    }
+    if (typeof value === 'string') {
+      setInputValue(value);
     }
   }, [value]);
   useEffect(() => {
@@ -46,38 +48,20 @@ const TextInput = ({
       return;
     }
 
-    const go = async () => {
-      await sleep(7000);
-      const oldPlaceHolder =
-        placeholders[placeholderIndex % placeholders.length];
-      const newPlaceHolder =
-        placeholders[(placeholderIndex + 1) % placeholders.length];
-      const steps = oldPlaceHolder.length + newPlaceHolder.length + 4;
-      const stepSizeMs = 25;
-      for (let i = 0; i <= steps; i++) {
-        const shouldRemove = i <= oldPlaceHolder.length;
-        const shouldAdd = steps - i <= newPlaceHolder.length;
-        if (shouldRemove) {
-          setCurrentPlaceHolder(
-            oldPlaceHolder.substr(0, oldPlaceHolder.length - i)
-          );
-        } else if (shouldAdd) {
-          setCurrentPlaceHolder(
-            newPlaceHolder.substr(0, newPlaceHolder.length - (steps - i))
-          );
-        }
-        //  – it's ok in a for loop
-        // eslint-disable-next-line no-await-in-loop
-        await sleep(stepSizeMs);
-      }
-      setPlaceholderIndex((index) => index + (1 % placeholders.length));
-    };
-    go();
+    updatePlaceHolderDelayed(
+      placeholders,
+      placeholderIndex,
+      setPlaceholderIndex,
+      setCurrentPlaceHolder
+    );
   }, [placeholderIndex]);
 
+  const cleanInput = (val: string) => val.toLowerCase().replace(' ', '-');
   const submit = (submitValue) => {
-    setSubmitted(true);
-    onSubmit(submitValue);
+    if (!hasSubmitted) {
+      setSubmitted(true);
+    }
+    onSubmit(cleanInput(submitValue));
   };
   return (
     <Question>
@@ -91,7 +75,7 @@ const TextInput = ({
             if (hasSubmitted) {
               onSubmit(e.target.value);
             } else {
-              setInputValue(e.target.value);
+              setInputValue(cleanInput(e.target.value));
             }
           }}
           onKeyPress={(e) => e.key === 'Enter' && submit(inputValue)}
@@ -106,6 +90,38 @@ const TextInput = ({
       {description && <Description>{description}</Description>}
     </Question>
   );
+};
+
+const updatePlaceHolderDelayed = async (
+  placeholders: string[],
+  placeholderIndex: number,
+  setPlaceholderIndex: any,
+  setCurrentPlaceHolder: (value: string) => void,
+  delayMs: number = 7000
+) => {
+  await sleep(delayMs);
+  const oldPlaceHolder = placeholders[placeholderIndex % placeholders.length];
+  const newPlaceHolder =
+    placeholders[(placeholderIndex + 1) % placeholders.length];
+  const steps = oldPlaceHolder.length + newPlaceHolder.length + 4;
+  const stepSizeMs = 25;
+  for (let i = 0; i <= steps; i++) {
+    const shouldRemove = i <= oldPlaceHolder.length;
+    const shouldAdd = steps - i <= newPlaceHolder.length;
+    if (shouldRemove) {
+      setCurrentPlaceHolder(
+        oldPlaceHolder.substr(0, oldPlaceHolder.length - i)
+      );
+    } else if (shouldAdd) {
+      setCurrentPlaceHolder(
+        newPlaceHolder.substr(0, newPlaceHolder.length - (steps - i))
+      );
+    }
+    //  – it's ok in a for loop
+    // eslint-disable-next-line no-await-in-loop
+    await sleep(stepSizeMs);
+  }
+  setPlaceholderIndex((index) => index + (1 % placeholders.length));
 };
 
 export default TextInput;
