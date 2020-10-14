82b235747974d9ef7a37ec8f1f0d483c5173a5bc
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-14 19:14:18 +0200

    terraform: support dns, redirect and certificates
    Fixes #5

--- utils/terraform.ts
+++ utils/terraform.ts
@@ -1,4 +1,7 @@
+/* eslint-disable camelcase */
+
 import { BUCKET_TYPE, getInputDescription, getOutput, INPUT } from '../enums';
+import { domainIsApex, domainIsWWW, getApexDomain } from './domain';
 
 export type TerraformProps = {
   webApp: boolean;
@@ -18,51 +21,301 @@ type TerraformPackage = {
   description?: string;
 };
 
-const getMainTfContent = ({
+type ModuleSpec = {
+  name: string;
+  source: string;
+  parameters: {
+    [key: string]: string;
+  };
+};
+
+type BucketProp = {
+  redirect: string | null;
+  main: string;
+  staging: string | null;
+};
+
+const getBucketModuleNames = ({
   webApp,
   staging,
-  shared,
-  staticPage,
-  region,
-  bucketName,
-  errorPath,
-}: TerraformProps): string[] => {
-  const source = webApp
-    ? 'git::https://github.com/tomfa/terraform.git//webapp'
-    : 'git::https://github.com/tomfa/terraform.git//files';
-
-  const parameters: { [param: string]: string } = {
-    bucket_name: bucketName ? `"${bucketName}"` : `var.${INPUT.BUCKET_NAME}`,
-    aws_region: region ? `"${region}"` : `var.${INPUT.AWS_REGION}`,
+  forwardingBucket,
+}: TerraformProps): BucketProp => {
+  const genericBucketName = webApp ? 'web-app' : 'file-storage';
+  const mainBucket = staging
+    ? `${genericBucketName}-production`
+    : genericBucketName;
+  const stagingBucket = staging ? `${genericBucketName}-staging` : null;
+  return {
+    staging: stagingBucket,
+    main: mainBucket,
+    redirect: forwardingBucket ? 'redirect' : null,
   };
-  if (webApp) {
-    parameters.error_path =
-      staticPage && errorPath ? `"/${errorPath}"` : '"/index.html"';
-    parameters.error_code = staticPage ? '404' : '200';
-  } else {
-    parameters.acl = shared ? '"public-read"' : '"private"';
+};
+
+const getBucketDomains = ({
+  bucketName,
+  staging,
+  forwardingBucket,
+}: TerraformProps): BucketProp => {
+  const stagingBucket = staging ? getStagingDomain(bucketName) : null;
+  return {
+    staging: stagingBucket,
+    main: bucketName,
+    redirect: forwardingBucket || null,
+  };
+};
+
+type Target = { target_name: string; target_zone_id: string };
+type BucketTargets = {
+  redirect: Target | null;
+  main: Target;
+  staging: Target | null;
+};
+const getBucketTargetRefs = (props: TerraformProps): BucketTargets => {
+  const moduleNames = getBucketModuleNames(props);
+  const s3TargetNameAttr = 'BUCKET_WEBSITE_DOMAIN';
+  const s3TargetZoneIdAttr = 'BUCKET_ZONE_ID';
+  const cloudFrontTargetNameAttr = 'CLOUDFRONT_URL';
+  const cloudFrontTargetZoneIdAttr = 'CLOUDFRONT_ZONE_ID';
+  const getTarget = (moduleName: string, usesS3: boolean): null | Target => {
+    if (!moduleName) {
+      return null;
+    }
+    return {
+      target_name:
+        `module.${moduleName}.` +
+        (usesS3 ? s3TargetNameAttr : cloudFrontTargetNameAttr),
+      target_zone_id:
+        `module.${moduleName}.` +
+        (usesS3 ? s3TargetZoneIdAttr : cloudFrontTargetZoneIdAttr),
+    };
+  };
+  return {
+    staging: getTarget(moduleNames.staging, !props.webApp),
+    main: getTarget(moduleNames.main, !props.webApp),
+    redirect: getTarget(moduleNames.redirect, true),
+  };
+};
+
+const getDomainTfContent = ({
+  configureDns,
+  bucketName,
+}: TerraformProps): ModuleSpec[] => {
+  if (!configureDns) {
+    return [];
+  }
+  const apexDomain = getApexDomain(bucketName);
+  return [
+    {
+      name: 'domain',
+      source: 'git::https://github.com/tomfa/terraform.git//domain',
+      parameters: { domain: `"${apexDomain}"` },
+    },
+  ];
+};
+
+const getDomainRecordTfContent = (props: TerraformProps): ModuleSpec[] => {
+  const { configureDns, forwardingBucket, staging } = props;
+  if (!configureDns) {
+    return [];
   }
 
-  const genericBucketName = webApp ? 'web-app' : 'file-storage';
-  const modules = staging
-    ? [
-        {
-          name: `${genericBucketName}-production`,
-          parameters,
-        },
-        {
-          name: `${genericBucketName}-staging`,
-          parameters: {
-            ...parameters,
-            bucket_name: bucketName
-              ? `"${bucketName}.staging"`
-              : `"\\$\{var.bucket_name}.staging"`,
-          },
-        },
-      ]
-    : [{ name: genericBucketName, parameters }];
+  const domains = getBucketDomains(props);
+  const targets = getBucketTargetRefs(props);
 
+  const tfContent: ModuleSpec[] = [
+    {
+      name: 'bucket_record',
+      source: 'git::https://github.com/tomfa/terraform.git//alias_record',
+      parameters: {
+        dns_zone_id: `module.domain.DOMAIN_ZONE_ID`,
+        domain: `"${domains.main}"`,
+        ...targets.main,
+      },
+    },
+  ];
+
+  if (staging) {
+    tfContent.push({
+      name: 'staging_record',
+      source: 'git::https://github.com/tomfa/terraform.git//alias_record',
+      parameters: {
+        dns_zone_id: `module.domain.DOMAIN_ZONE_ID`,
+        domain: `"${domains.staging}"`,
+        ...targets.staging,
+      },
+    });
+  }
+
+  if (forwardingBucket) {
+    tfContent.push({
+      name: 'redirect_record',
+      source: 'git::https://github.com/tomfa/terraform.git//alias_record',
+      parameters: {
+        dns_zone_id: `module.domain.DOMAIN_ZONE_ID`,
+        domain: `"${domains.redirect}"`,
+        ...targets.redirect,
+      },
+    });
+  }
+
+  return tfContent;
+};
+
+const getCertificateTfContent = ({
+  createCertificates,
+  bucketName,
+}: TerraformProps): ModuleSpec[] => {
+  if (!createCertificates) {
+    return [];
+  }
+  const apexDomain = getApexDomain(bucketName);
+  const alternativeMames = [`*.${apexDomain}`];
+  if (bucketName !== apexDomain) {
+    alternativeMames.push(bucketName);
+  }
+  alternativeMames.push(getStagingDomain(bucketName));
+  const alternativeNameString =
+    alternativeMames.length > 0 ? `["${alternativeMames.join('","')}"]` : '[]';
+  return [
+    {
+      name: 'certificate',
+      source: 'git::https://github.com/tomfa/terraform.git//certificate',
+      parameters: {
+        domain: `"${apexDomain}"`,
+        alternative_names: `${alternativeNameString}`,
+      },
+    },
+  ];
+};
+
+const getRedirectBucketTfContent = ({
+  forwardingBucket,
+  bucketName,
+  region,
+}: TerraformProps): ModuleSpec[] => {
+  if (!forwardingBucket) {
+    return [];
+  }
+  return [
+    {
+      name: 'redirect',
+      source: `"git::https://github.com/tomfa/terraform.git//redirect"`,
+      parameters: {
+        bucket_name: `"${forwardingBucket}"`,
+        redirect_url: `"${bucketName}"`,
+        aws_region: `"${region}"`,
+      },
+    },
+  ];
+};
+
+const getFileStorageBucketTfContent = (props: TerraformProps): ModuleSpec[] => {
+  if (props.webApp) {
+    return [];
+  }
+  const tfContent: ModuleSpec[] = [];
+  const names = getBucketModuleNames(props);
+  const { bucketName, region, shared } = props;
+  tfContent.push({
+    name: names.main,
+    source: 'git::https://github.com/tomfa/terraform.git//files',
+    parameters: {
+      bucket_name: bucketName ? `"${bucketName}"` : `var.${INPUT.BUCKET_NAME}`,
+      aws_region: region ? `"${region}"` : `var.${INPUT.AWS_REGION}`,
+      acl: shared ? '"public-read"' : '"private"',
+    },
+  });
+  if (props.staging) {
+    tfContent.push({
+      name: names.staging,
+      source: 'git::https://github.com/tomfa/terraform.git//files',
+      parameters: {
+        bucket_name: `"${getStagingBucketName(bucketName)}"`,
+        aws_region: region ? `"${region}"` : `var.${INPUT.AWS_REGION}`,
+        acl: shared ? '"public-read"' : '"private"',
+      },
+    });
+  }
+  return tfContent;
+};
+
+const getWebAppBucketTfContent = (props: TerraformProps): ModuleSpec[] => {
+  if (!props.webApp) {
+    return [];
+  }
+  const names = getBucketModuleNames(props);
+  const { bucketName, region, staticPage, errorPath } = props;
+  const bucketSpec: ModuleSpec = {
+    name: names.main,
+    source: 'git::https://github.com/tomfa/terraform.git//webapp',
+    parameters: {
+      bucket_name: bucketName ? `"${bucketName}"` : `var.${INPUT.BUCKET_NAME}`,
+      aws_region: region ? `"${region}"` : `var.${INPUT.AWS_REGION}`,
+      error_path: staticPage && errorPath ? `"/${errorPath}"` : '"/index.html"',
+      error_code: staticPage ? '404' : '200',
+    },
+  };
+
+  if (props.createCertificates) {
+    bucketSpec.parameters.certificate_arn =
+      'module.certificate.CERTIFICATE_ARN';
+  }
+  if (props.configureDns && bucketName) {
+    bucketSpec.parameters.domain_aliases = `["${bucketName}"]`;
+  }
+
+  if (!props.staging) {
+    return [bucketSpec];
+  }
+
+  const stagingSpec: ModuleSpec = {
+    ...bucketSpec,
+    name: names.staging,
+    parameters: {
+      ...bucketSpec.parameters,
+      bucket_name: `"${getStagingBucketName(bucketName)}"`,
+    },
+  };
+
+  if (props.configureDns && bucketName) {
+    stagingSpec.parameters.domain_aliases = `["${getStagingDomain(
+      bucketName
+    )}"]`;
+  }
+
+  return [bucketSpec, stagingSpec];
+};
+
+const getStagingBucketName = (bucketName: string): string => {
+  if (!bucketName) {
+    return `staging.\\$\{var.bucket_name}`;
+  }
+  return getStagingDomain(bucketName);
+};
+
+const getStagingDomain = (domain: string): string => {
+  if (domainIsApex(domain) || domainIsWWW(domain)) {
+    const apex = getApexDomain(domain);
+    return `staging.${apex}`;
+  }
+  return `staging-${domain}`;
+};
+
+const getMainTfContent = (props: TerraformProps): string[] => {
+  const modules: ModuleSpec[] = [];
+  modules.push(...getDomainTfContent(props));
+  modules.push(...getDomainRecordTfContent(props));
+  modules.push(...getCertificateTfContent(props));
+  if (props.webApp) {
+    modules.push(...getWebAppBucketTfContent(props));
+  } else {
+    modules.push(...getFileStorageBucketTfContent(props));
+  }
+  modules.push(...getRedirectBucketTfContent(props));
+  modules.push(...getCertificateTfContent(props));
   const lines: string[] = [];
+
   const isUsed = (inputVariable: string): boolean =>
     !!modules.find(
       (m) =>
@@ -85,7 +338,7 @@ const getMainTfContent = ({
   modules.forEach((module) => {
     lines.push('');
     lines.push(`module "${module.name}" {`);
-    lines.push(`  source = "${source}"`);
+    lines.push(`  source = "${module.source}"`);
     Object.entries(module.parameters).forEach(([key, value]) => {
       lines.push(`  ${key} = ${value}`);
     });
@@ -93,8 +346,8 @@ const getMainTfContent = ({
   });
   lines.push('');
   getOutput({
-    bucketType: webApp ? BUCKET_TYPE.WEBAPP : BUCKET_TYPE.FILE_STORAGE,
-    hasStaging: staging,
+    bucketType: props.webApp ? BUCKET_TYPE.WEBAPP : BUCKET_TYPE.FILE_STORAGE,
+    hasStaging: props.staging,
   }).forEach((output) => {
     lines.push(`output "${output.label}" {`);
     lines.push(`  value = ${output.value}`);
@@ -115,9 +368,10 @@ const getTerraPackageDescription = ({
   const iamUserInfo = staging
     ? 'Two sets of AWS keys will be created that are able to upload to the buckets. One for test and one for production environment'
     : 'A set of AWS keys will be created that is able to upload to the bucket';
+  const generatedKeysInfo = `${iamUserInfo}. Generated keys will be shown as output in the terminal. These can be used to upload new versions of the app to the bucket.`;
   return `Once run, it will create ${count} S3 bucket${
     webApp ? ' + Cloudfront' : ''
-  } in ${region}, configured ${usecase}. ${iamUserInfo}. Generated keys will be shown as output in the terminal.`;
+  } in ${region}, configured ${usecase}. ${generatedKeysInfo}`;
 };
 
 const getUseCaseDescription = ({
