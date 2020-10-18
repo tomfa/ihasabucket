ef139fcdc1db3baed7f806ba00f9d87917f814d7
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-10-18 11:23:03 +0200

    feature(terraform): set up DNS pointers for cert validation
    FIXES #20

--- utils/terraform/certificate.ts
+++ utils/terraform/certificate.ts
@@ -5,6 +5,7 @@ import { getStagingDomain } from './names';
 export const getCertificateTfContent = ({
   createCertificates,
   bucketName,
+  configureDns,
 }: TerraformProps): ModuleSpec[] => {
   if (!createCertificates) {
     return [];
@@ -17,14 +18,17 @@ export const getCertificateTfContent = ({
   alternativeMames.push(getStagingDomain(bucketName));
   const alternativeNameString =
     alternativeMames.length > 0 ? `["${alternativeMames.join('","')}"]` : '[]';
-  return [
-    {
-      name: 'certificate',
-      source: 'git::https://github.com/tomfa/terraform.git//certificate',
-      parameters: {
-        domain: `"${apexDomain}"`,
-        alternative_names: `${alternativeNameString}`,
-      },
+  const certificateModule: ModuleSpec = {
+    name: 'certificate',
+    source: 'git::https://github.com/tomfa/terraform.git//certificate',
+    parameters: {
+      domain: `"${apexDomain}"`,
+      alternative_names: `${alternativeNameString}`,
     },
-  ];
+  };
+  if (configureDns) {
+    certificateModule.parameters.route53_zone_id =
+      'module.domain.DOMAIN_ZONE_ID';
+  }
+  return [certificateModule];
 };
