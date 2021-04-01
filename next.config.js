7d0c414dc3d1a249c5051bea4801e3864078c162
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-04-01 17:47:09 +0200

    next-config: export with trailing slash
    This will work well with S3/Cloudfront when having
    more than one page.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { findLastCommitSha } = require('./gitBuildId');
 
 module.exports = {
   distDir: 'build',
+  trailingSlash: true,
   generateBuildId: async () => {
     const commitSha = await findLastCommitSha();
     return commitSha.substr(0, 7);
